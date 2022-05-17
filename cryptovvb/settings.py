3a1257bca7ef376945a3e47c09c4f7230e4a791a
Никита Кульпинов <ganggstand@gmail.com>
2022-05-17 23:54:45 +0300

    Update settings.py

--- cryptovvb/settings.py
+++ cryptovvb/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*']
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "vvbcommand@gmail.com"
-EMAIL_HOST_PASSWORD = "xpsrjlvbftcijhps"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 # Application definition
